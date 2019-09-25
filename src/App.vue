b7594a68b7c902d4046a788f53867ebf86ed0446
Your Name <you@example.com>
2019-09-25 20:45:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <TasksList :tasks="tasks"/>
+  </div>
+</template>
+
+<script>
+import TasksList from './components/TasksList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TasksList
+  },
+  data() {
+    return {
+      tasks: [],
+    }
+  },
+  methods: {
+    getTasks: async function() {
+      try {
+        const data = await fetch('http://localhost:8000/api/tasks');
+        this.tasks = await data.json();
+      } catch (error) {
+        throw error;
+      }
+
+    }
+  },
+  mounted() {
+    this.getTasks();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <pre>{{tasks}}</pre>
+    <p></p>
+    <table class="table" >
+      <thead>
+        <tr >
+          <th scope="col"></th>
+          <th scope="col"></th>
+          <th scope="col"></th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks.tasks" :key="task.id">
+          <th scope="row">{{task.id}}</th>
+          <td>{{task.name}}</td>
+          <td>{{task.descripcion}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <button type=""></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['tasks']
+  
+}
+</script>
